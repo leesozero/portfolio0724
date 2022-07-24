a6b9b635efad28ecf307c6994be8c0e94bda9802
leesozero <leesozero@gmail.com>
2022-07-24 17:15:39 +0900

    오늘배운것 마지막

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 $(document).ready(
     function(){
-      
+        
+      $('#banner').css('opacity',0) //투명하게 처리해라
+
+      $('#banner').animate({ // 2초를 들여서 점점 나타나라
+        "opacity" : 1
+
+      },2000,function(){
+        $(this).html('애니메이션 끝나고 나는 나타나겠지')
+      })
         
     }
 )  
